7a28803e7a09a9a492d335974a889c91876ec023
木下喜雄 <kinoshitayoshio@kinoshitayoshio-no-MacBook-Air.local>
2019-02-27 15:55:44 +0900

    inside

--- ch3/3.3sorted.py
+++ ch3/3.3sorted.py
@@ -9,3 +9,9 @@ print(sorted(a, reverse=True))
 s = ["banana", "apple", "peach"]
 print(sorted(s))
 print(sorted(s, reverse=True))
+
+# 内包表記
+# [式 for 要素名 in リスト]
+
+print([x * 2 for x in [1, 2, 3,4]])
+print([x**2 for x in range(5)])
