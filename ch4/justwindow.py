a72b4072522e82ed5d7bc88d195f7d431204b925
木下喜雄 <kinoshitayoshio@kinoshitayoshio-no-MacBook-Air.local>
2019-03-03 16:33:55 +0900

    add files

--- ch4/justwindow.py
+++ ch4/justwindow.py
@@ -9,16 +9,24 @@ pygame.display.set_caption("Just Window")
 
 def main():
     """ main routine """
+    sysfont = pygame.font.SysFont(None, 36)
+    counter = 0
 
     while True:
-        SURFACE.fill((255, 255, 255))
-
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
+        counter += 1
+        SURFACE.fill((0, 0, 0))
+
+        count_image = sysfont.render(
+            "count is {}".format(counter), True, (255, 255, 225))
+
+        SURFACE.blit(count_image, (50, 50))
         pygame.display.update()
+
 
 if __name__ == '__main__':
     main()
